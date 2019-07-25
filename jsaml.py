42662c8801a5dceb8925d86a4fbbaccf36730415
Tim Hawes <tim@selfdidactic.com>
2019-07-25 11:08:09 -0400

    Initial commit

---
+++ jsaml.py
@@ -0,0 +1,12 @@
+""" jsaml
+Simple program to convert json to yaml and yaml to json.
+"""
+
+import json
+import yaml
+
+class Jsaml:
+    def __init__(self, filepath):
+        pass
+
+if __name__ == "__main__":
